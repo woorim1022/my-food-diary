5c4430d5c05c3706fc0c7a0b179d40ecd13bc316
cano721 <github.com/cano721>
2021-02-03 11:30:58 +0900

    Update app

--- frame/ingredientapp/ingredientapp_sql.py
+++ frame/ingredientapp/ingredientapp_sql.py
@@ -3,15 +3,19 @@ class Sql:
                     INNER JOIN ingr i ON ui.i_id = i.i_id
                     INNER JOIN ingr_ct ic ON i.ic_id = ic.ic_id
                     INNER JOIN ingr_ct icp ON ic.icp_id = icp.ic_id""";
+
     user_avoidlist = """SELECT icp.ic_name, ic.ic_name, i.i_name,ui.ui_exdate FROM users_avoid ua
                     INNER JOIN users_ingr ui ON ua.i_id = ui.i_id
                     INNER JOIN ingr i ON ua.i_id = i.i_id
                     INNER JOIN ingr_ct ic ON i.ic_id = ic.ic_id
                     INNER JOIN ingr_ct icp ON ic.icp_id = icp.ic_id""";
 
-    ingrlistone = "SELECT * FROM ingr WHERE id=%d";
-    ingrinsert = "INSERT INTO ingr VALUE (%d,%d,'%s')"
+    user_ingrinsert = "INSERT INTO users_ingr VALUE (null,'%s',%d,'%s','%s')"
 
+    ingrlist = """SELECT icp.ic_name, icp.ic_id, ic.ic_name, ic.ic_id, i.i_name, i.i_id  FROM ingr i
+                INNER JOIN ingr_ct ic ON i.ic_id = ic.ic_id
+                INNER JOIN ingr_ct icp ON ic.icp_id = icp.ic_id""";
+    ingrlistone = "SELECT * FROM ingr WHERE id=%d";
     ingr_ctlist = "SELECT * FROM ingr_ct";
     ingr_ctlistone = "SELECT * FROM ingr_ct WHERE id=%d";
     ingr_ctinsert = "INSERT INTO ingr_ct VALUE (%d,'%s',%d)"
--- frame/recipeapp/recipeapp_value.py
+++ frame/recipeapp/recipeapp_value.py
@@ -9,3 +9,17 @@
 #     def __str__(self):
 #         return self.u_id + ' ' + self.u_pwd + ' ' + self.u_name + ' '\
 #                 +self.u_name + ' ' + str(self.u_age) + ' ';
+
+class User_Ingr:
+    def __init__(self,ui_id,ui_regdate,icp_name,ic_name,i_name,ui_exdate):
+        self.ui_id = ui_id;
+        self.ui_regdate = ui_regdate;
+        self.icp_name = icp_name;
+        self.ic_name = ic_name;
+        self.i_name = i_name;
+        self.ui_exdate = ui_exdate;
+
+    def __str__(self):
+        return str(self.ui_id) + ' ' + str(self.ui_regdate) + ' ' +\
+               self.icp_name + ' ' + self.ic_name + ' ' + self.i_name + ' ' +\
+               str(self.ui_exdate) + ' ';
